99a3dbbf2b3a9dba0d996c44d0af67d2c52b8157
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-04 15:29:00 +0200

    wip driver

--- app/Drivers/ActionsDriver.php
+++ app/Drivers/ActionsDriver.php
@@ -5,13 +5,16 @@ namespace App\Drivers;
 use App\Driver;
 use Carbon\Carbon;
 use App\Colors;
+use Illuminate\Support\Facades\Http;
 
 class ActionsDriver extends Driver
 {
     public $embed;
 
     protected const VALIDATION_RULES = [
-        'id' => 'required|integer',
+        'organisation' => 'required|string',
+        'repository' => 'required|string',
+        'workflow_id' => 'required|integer',
     ];
 
     public function __construct(array $validated)
@@ -21,6 +24,8 @@ class ActionsDriver extends Driver
 
     protected function create(array $validated): array
     {
+        $payload = $this->getGitHubPayload($validated);
+
         /**
          * Author:
          *  - icon_url
@@ -35,15 +40,67 @@ class ActionsDriver extends Driver
         return [
             'author' => [
                 'icon_url' => '',
-                'name' => '',
+                'name' => $payload->head_commit->author->name,
                 'url' => '',
             ],
-            'color' => Colors::PASSED(),
-            'description' => '',
+            'color' => $this->getBuildColor($payload),
+            'description' => $this->getBuildDescription($payload),
             'timestamp' => Carbon::now(),
-            'title' => '',
-            'url' => '',
+            'title' => $this->getBuildTitle($payload),
+            'url' => $payload->html_url,
         ];
+    }
+
+    protected function getGitHubUri(array $validated): string
+    {
+        return 'https://api.github.com/repos/'
+            . $validated['organisation'] . '/'
+            . $validated['repository'] . '/'
+            . 'actions/workflows/'
+            . $validated['workflow_id'] . '/'
+            . 'runs?per_page=1';
+    }
+
+    protected function getGitHubPayload(array $validated): object
+    {
+        $response = Http::get(
+            $this->getGitHubUri($validated)
+        );
+
+        return json_decode($response->body())
+            ->workflow_runs[0];
+    }
+
+    protected function getBuildColor(object $payload): Colors
+    {
+        switch ($payload->conclusion) {
+            case 'success':
+                return Colors::PASSED();
+
+            case 'failure':
+                return Colors::FAILED();
+
+            default:
+                Colors::CANCELED();
+        }
+    }
+
+    protected function getBuildDescription(object $payload): string
+    {
+        $gitHash = substr($payload->head_sha, 0, 7);
+        $commitUrl = $payload->html_url;
+        $commitName = $payload->head_commit->message;
+
+        return "[{$gitHash}]({$commitUrl}) {$commitName}";
+    }
+
+    protected function getBuildTitle(object $payload): string
+    {
+        $repoInfo = "[{$payload->repository->full_name}]:{$payload->head_branch}";
+        $number = $payload->run_number;
+        $status = $payload->conclusion;
+
+        return "{$repoInfo} Build #{$number} {$status}";
     }
 
     public function wasSuccessful(): bool
