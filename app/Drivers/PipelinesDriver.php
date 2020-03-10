d8da64ae5dca9c708b0d78469d7b937ad24a8b7f
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-10 11:07:07 +0200

    wip

--- app/Drivers/PipelinesDriver.php
+++ app/Drivers/PipelinesDriver.php
@@ -3,8 +3,34 @@
 namespace App\Drivers;
 
 use App\Driver;
+use GitHub;
 
-class PipelinesDriver //extends Driver
+class PipelinesDriver extends Driver
 {
-    //
+    protected $validated;
+    protected $commit;
+
+    protected const VALIDATION_RULES = [
+        'resource.finishTime' => 'required|date',
+        'resource.status' => 'required|string',
+        'resource.id' => 'required|string',
+        'resource.url' => 'required|url'
+    ];
+
+    protected const STATUSES_CANCELED = 'canceled';
+    protected const STATUSES_FAILED = 'broken|failed|stillFailing';
+    protected const STATUSES_PASSED = 'fixed|passed|succeeded';
+    protected const STATUSES_PENDING = 'pending';
+
+    public function __construct(array $validated)
+    {
+        $this->validated = $validated;
+
+        $orgAndRepo = '';
+
+        $this->commitHash = '';
+        $this->branch = '';
+
+        $this->commit = (new GitHub)->getCommitDetails($orgAndRepo, $this->commitHash);
+    }
 }
