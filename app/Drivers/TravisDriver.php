dae6cdd5955b57182521635b58af5e01f88e71b9
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-10 16:08:58 +0200

    Fix errors

--- app/Drivers/TravisDriver.php
+++ app/Drivers/TravisDriver.php
@@ -35,11 +35,11 @@ class TravisDriver extends Driver
         $this->validated = $validated;
         $this->commit = (new GitHub)->getCommitDetails(
             $this->getFullRepositoryName(),
-            $validated->commit
+            $validated['commit']
         );
 
-        $this->commitHash = $this->validated->commit;
-        $this->branch = $this->validated->branch;
+        $this->commitHash = $this->validated['commit'];
+        $this->branch = $this->validated['branch'];
 
         $this->embed = $this->create();
     }
@@ -49,30 +49,30 @@ class TravisDriver extends Driver
         return [
             'author' => [
                 'icon_url' => $this->commit->author->avatar_url,
-                'name' => $this->validated->author_name,
+                'name' => $this->validated['author_name'],
                 'url' => $this->commit->author->html_url,
             ],
             'color' => $this->getBuildColor($this->payload->state),
             'description' => $this->getBuildDescription(
-                $this->validated->commit,
-                $this->validated->compare_url,
-                $this->validated->message
+                $this->validated['commit'],
+                $this->validated['compare_url'],
+                $this->validated['message']
             ),
             'timestamp' => $this->validated->finished_at,
             'title' => $this->getBuildTitle(
                 $this->getFullRepositoryName(),
-                $this->validated->branch,
-                $this->validated->number,
-                $this->validated->status_message
+                $this->validated['branch'],
+                $this->validated['number'],
+                $this->validated['status_message']
             ),
-            'url' => $this->validated->build_url,
+            'url' => $this->validated['build_url'],
         ];
     }
 
     protected function getFullRepositoryName()
     {
-        return $this->validated->repository->owner_name
+        return $this->validated['repository']['owner_name']
             . '/'
-            . $this->validated->repository->name;
+            . $this->validated['repository']['name'];
     }
 }
