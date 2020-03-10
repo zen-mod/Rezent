df5e6f63be35d9cd3f3b8a6d121ce4d7b7b216c1
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-10 18:00:55 +0200

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,23 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="card">
+        <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+        <div class="card-body">
+            {{-- Rate Limit --}}
 
-                    <passport-clients></passport-clients>
-                    <passport-authorized-clients></passport-authorized-clients>
-                    <passport-personal-access-tokens></passport-personal-access-tokens>
-                </div>
-            </div>
+            {{-- 10 latest builds --}}
+
+            {{-- List projects --}}
+
+            {{-- Create new project --}}
         </div>
     </div>
 </div>
