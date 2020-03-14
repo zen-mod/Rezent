71f22a215e29e5efc606206a12bdb9beab08d4d8
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-14 09:53:49 +0200

    Added display hash function for Build

--- app/Build.php
+++ app/Build.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Build extends Model
 {
     protected $fillable = ['commit_hash', 'branch', 'successful'];
+
+    public function displayHash()
+    {
+        return substr($this->commit_hash, 0, 7);
+    }
 }
